7d7e8e87d73e40751bf487ff16a5ec7cc7d718c4
zsdostar <zsdzzydos@gmail.com>
2019-10-22 16:56:43 +0800

    extra space

--- loadbalance/roundrobin.go
+++ loadbalance/roundrobin.go
@@ -22,7 +22,7 @@ func (r *RoundRobinBalance) Select(ctx context.Context, nodes []*registry.Node) 
 	}
 
 	r.index = (r.index + 1) % len(nodes)
-	node = nodes[r.i ndex]
+	node = nodes[r.index]
 
 	return
 }
